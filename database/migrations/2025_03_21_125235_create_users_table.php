2cdd0b892e1a45286d3fceb5c783824b6601af4c
Romy <110410235+romynurwidiantodafalah@users.noreply.github.com>
2025-03-21 20:22:43 +0700

    first commit-bengkod

---
+++ database/migrations/2025_03_21_125235_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama', 255);
+            $table->string('alamat', 255)->nullable();
+            $table->string('no_hp', 50);
+            $table->string('email', 50)->unique();
+            $table->string('role', 50)->default('user');
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
